1da264fe703ace9b5fe04b5036941080ce184372
jorge nieto <jorge.nieto@babel.es>
2016-02-18 14:01:40 +0100

    Configuracion general del proyecto con grunt y less

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+ module.exports = function (grunt) {
+ 	//config de grunt
+ 	var settings = {
+ 		less: {
+ 			style: {
+ 				files: { //archivos a compilar
+ 					"style.css" : "less/style.less" //destino : origen
+ 				}
+
+ 			}
+ 		}, //less
+
+ 		watch: {
+ 			styles: {
+ 				files: { // observar diferentes archivos en diferentes carpetas
+ 					files: ['less/*.less'], // cuando cambie cualquier archivo .less
+ 					tasks: ['less'], // ejecuta la tarea less  
+ 					options: {
+ 						spawn: false //para que no se quede tostado, ir mas rapido
+ 					}
+ 				}
+ 			}
+
+ 		} //watch
+
+ 	};
+
+ 	//cargamos cofiguracion de grunt
+	grunt.initConfig(settings);
+
+	//cargamos los plugins
+	grunt.loadNpmTasks('grunt-contrib-less');
+ 	grunt.loadNpmTasks('grunt-contrib-watch');
+
+ 	//definimos las tareas disponibles para grunt-cli
+ 	//compila less y se pone a observar y cada cambio compila
+ 	grunt.registerTask('default', ['less', 'watch']);
+ 	//solo coge y compila less
+ 	grunt.registerTask('producction', ['less']);
+ };
--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # SeriesDB
 An awesome clone of The Movies Database to learn HTML5, CCS3 and JavaScript
 
+## Setup de entorno de desarrollo 
+Debes tener instalado [nodejs]()
+con *npm* instalar *grunt* y *less* utilizando:
+
+```
+$sudo npm install -g grunt-cli less
+```
+
+A continuacion clona el repositorio e instala las dependendias para grunt `npm install` desde la carpeta del proyecto.
+
+Despues de arrancar *grunt*, utilizando el comando `grunt`.
+
 ##change log
 
 ###v 0.1.0 - 2016-02-17
